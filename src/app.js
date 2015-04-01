99e8fc3b1d236cfcae4bada35300fb2dc783a566
chikeichan <chikeichan@gmail.com>
2015-03-31 21:59:10 -0700

    Added some instructions to help get started

---
+++ src/app.js
@@ -0,0 +1,30 @@
+/*
+  Input      : # of row, # of columns
+  Output     : Append x rows and y columns to table#playfield with proper class names
+  Example    : drawGrid(4,2) --> Append the follow to table#playfield
+                 <tr class='0'>
+                   <td class='0'></td>
+                   <td class='1'></td>
+                   <td class='2'></td>
+                   <td class='3'></td>
+                 </tr> 
+                 <tr class='2'>
+                   <td class='0'></td>
+                   <td class='1'></td>
+                   <td class='2'></td>
+                   <td class='3'></td>
+                 </tr>
+*/
+function drawGrid(x,y){
+  //Start here...
+};
+
+/*
+  Input      : Arrays of coordinates, color
+  Output     : Fill each table cell referenced by the array of coordinates in the specific color
+  Example    : fillCells([{x:1, y:1}, {x:1,y:2}], 'black') -->
+                 'tr.1 > td.1' and 'tr.1 > td.2' filled with color black
+*/
+function fillCells(array, color){
+  //Start here...
+};
