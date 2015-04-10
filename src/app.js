fe08cc165cafb4049e9f58f22d716bdb3029f426
ChandlerHo <chchuho@gmail.com>
2015-04-09 21:43:48 -0700

    Generate shape added

--- src/app.js
+++ src/app.js
@@ -48,16 +48,95 @@ function fillCells(array, color){
   }
 
 };
-//hello
+
+/*
+  Input : None
+  Output: Generate tetrismino's coordinatates
+  Example: Generate() produces an array object output with 4 coordinates
+*/
+
+function generate_Shape() {
+  var origin= { x : 0, y : 4};
+
+  //coordinates for six types of block
+
+  var square = [ 
+    { x : origin.x, y : origin.y },
+    { x : origin.x, y : (origin.y + 1) },
+    { x : (origin.x + 1) , y : origin.y },
+    { x : (origin.x + 1), y : (origin.y +1) }
+  ];
+
+  var L_Shape = [
+    { x : origin.x, y : origin.y },
+    { x : (origin.x+1), y : (origin.y) },
+    { x : (origin.x+2), y : (origin.y) },
+    { x : ( origin.x+2), y: (origin.y+1) }
+  ];
+
+  var bar = [
+    { x : origin.x, y : origin.y },
+    { x : (origin.x+1), y : (origin.y) },
+    { x : (origin.x+2), y : (origin.y) },
+    { x : (origin.x+3), y : (origin.y) }
+  ];
+  
+  var left_Shape = [
+    { x : origin.x, y : origin.y },
+    { x : (origin.x+1), y : (origin.y) },
+    { x : (origin.x+1), y : (origin.y+1) },
+    { x : (origin.x+2), y : (origin.y+1) }
+    ];
+ 
+  var right_Shape = [
+    { x : (origin.x), y : (origin.y+1) },
+    { x : (origin.x+1), y : (origin.y+1) },
+    { x : (origin.x+1), y : (origin.y) },
+    { x : (origin.x+2), y : (origin.y) }
+    ];
+
+  var top_Shape = [
+  { x : (origin.x), y : (origin.y) },
+  { x : (origin.x+1), y : (origin.y) },
+  { x : (origin.x+1), y : (origin.y-1) },
+  { x : (origin.x+1), y : (origin.y+1) }
+  ];
+
+  if(Math.random() < 0.166 && Math.random() > 0) {
+    return square;
+  }else if(Math.random() < 0.333 && Math.random() > 0.166) { 
+    return L_Shape;
+  }else if(Math.random() < 0.5 && Math.random() > 0.333) {
+    return left_Shape;
+  }else if(Math.random() < 0.666 && Math.random() > 0.5) {
+    return right_Shape;
+  }else if(Math.random() < 0.833 && Math.random() > 0.666) {
+    return top_Shape;
+  }else{
+    return bar;
+  }
+
+}
+
+$(document).keydown(function(e){
+  if (e.keyCode == 37) {
+  };
+});
+
+
+
 
 var input = [
-  { x : 1, y : 1 },
-  { x : 1, y : 2 }
+  { x : 0, y : 4 },
+  { x : 0, y : 5 },
+  { x : 1, y : 4 },
+  { x : 1, y : 5 }
 ];
 
 var color = "black";
 
 $(document).ready(function(){
   drawGrid(22,10);
-  fillCells(input, color);
+  fillCells(generate_Shape(), color);
 });
+
